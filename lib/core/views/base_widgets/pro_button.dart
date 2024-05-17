1de0432b71713d7daa5093fe816e8613ea1658b5
skytreex <skytreexskytreextn33@gmail.com>
2024-05-17 12:24:57 +0530

    create button

---
+++ lib/core/views/base_widgets/pro_button.dart
@@ -0,0 +1,61 @@
+// elevated_button.dart
+import 'package:flutter/material.dart';
+
+abstract class ProElevatedButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+ 
+ static const ProElevatedButton;
+ 
+    //required Key key,
+    //required this.text,
+    //required this.onPressed,
+
+  const ProElevatedButton({super.key, required this.text, required this.onPressed}) {
+    
+   key,
+  required void Function()? onPressed,
+  void Function()? onLongPress,
+  void Function(bool)? onHover,
+  void Function(bool)? onFocusChange,
+  ButtonStyle? style,
+  FocusNode? focusNode,
+  bool autofocus = false,
+  Clip? clipBehavior,
+  WidgetStatesController? statesController,
+  required Widget? child,
+  IconAlignment iconAlignment = IconAlignment.start,
+})
+
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var key;
+    var proElevatedButton5 = ProElevatedButton(
+      //onPressed: onPressed,
+      //child: Text(text),
+
+       Key? key,
+  required, text: 'null', 
+  void Function()? onPressed,
+  void Function()? onLongPress,
+  void Function(bool)? onHover,
+  void Function(bool)? onFocusChange,
+  ButtonStyle? style,
+  FocusNode? focusNode,
+  bool autofocus = false,
+  Clip? clipBehavior,
+  WidgetStatesController? statesController,
+  required Widget? child,
+  IconAlignment iconAlignment = IconAlignment.start,
+});
+    var proElevatedButton4 = proElevatedButton5;
+    var proElevatedButton3 = proElevatedButton4;
+    var proElevatedButton2 = proElevatedButton3;
+    var proElevatedButton = proElevatedButton2;
+    return proElevatedButton
+    );
+  }
+}
