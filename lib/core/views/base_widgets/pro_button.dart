8f82c5a556f904b06eba5ae924b359792de746d7
rajajanarthanan <160276299+rajajanarthanan@users.noreply.github.com>
2024-05-17 13:00:43 +0530

    UpdatedPro_button.datrt

--- lib/core/views/base_widgets/pro_button.dart
+++ lib/core/views/base_widgets/pro_button.dart
@@ -1,56 +1,41 @@
 // elevated_button.dart
 import 'package:flutter/material.dart';
 
-abstract class Pro_ElevatedButton extends StatelessWidget {
- // final String text;
-  final VoidCallback onPressed;
+class Pro_ElevatedButton extends StatelessWidget {
+  Pro_ElevatedButton(
+      {Key? prokey,
+      required Text text,
+      required void Function()? onPressed,
+      void Function()? onLongPress,
+      void Function(bool)? onHover,
+      void Function(bool)? onFocusChange,
+      ButtonStyle? style,
+      FocusNode? focusNode,
+      bool autofocus = false,
+      Clip? clipBehavior,
+      WidgetStatesController? statesController,
+      required Widget? child,
+      IconAlignment iconAlignment = IconAlignment.start}) 
 
- 
- // static const Pro_ElevatedButton;
- 
-    //required Key key,
-    //required this.text,
-    //required this.onPressed,
+       @override
+      Widget build(BuildContext context) {
+      return ElevatedButton(
+          key = prokey,
+          child: text = text,
+          onPressed: onPressed,
+          onLongPress = onLongPress,
+          onHover = onHover,
+          onFocusChange = onFocusChange,
+          style: style = style,
+          focusNode: focusNode = focusNode,
+          autofocus: autofocus = autofocus,
+          clipBehavior: clipBehavior = clipBehavior,
+          statesController: statesController = statesController,
+          child = child,
+          iconAlignment: iconAlignment = iconAlignment);
+    }
 
-  const ProElevatedButton({super.key, required this.text, required this.onPressed}) {
-    
-   key?key
-  required void Function()? onPressed,
-  void Function()? onLongPress,
-  void Function(bool)? onHover,
-  void Function(bool)? onFocusChange,
-  ButtonStyle? style,
-  FocusNode? focusNode,
-  bool autofocus = false,
-  Clip? clipBehavior,
-  WidgetStatesController? statesController,
-  required Widget? child,
-  IconAlignment iconAlignment = IconAlignment.start,
-})
-
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var key;
-    var proElevatedButton5 = ProElevatedButton(
-      //onPressed: onPressed,
-      //child: Text(text),
-
-       Key? key,
-  required, text: 'null', 
-   required Function()? onPressed,
-  void Function()? onLongPress,
-  void Function(bool)? onHover,
-  void Function(bool)? onFocusChange,
-  ButtonStyle? style,
-  FocusNode? focusNode,
-  bool autofocus = false,
-  Clip? clipBehavior,
-  WidgetStatesController? statesController,
-  required Widget? child,
-  IconAlignment iconAlignment = IconAlignment.start,
-});
-   
-  }
 }
+   
+  
+
