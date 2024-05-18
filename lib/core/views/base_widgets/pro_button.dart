47a57f4e1d9a9731be5e0020e5644e364832964c
skytreex <skytreexskytreextn33@gmail.com>
2024-05-18 13:52:49 +0530

    add buttons

--- lib/core/views/base_widgets/pro_button.dart
+++ lib/core/views/base_widgets/pro_button.dart
@@ -5,6 +5,8 @@
 // extend widgets for base_view
 // text each view from base_view
 
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 
 class BaseWidgets {
@@ -55,4 +57,11 @@ class BaseWidgets {
       child: child,
     );
   }
+   static
+    
+   
+   }
+
+   )
+     
 }
